b3a4f7b012c610a721d4f8922d365d8d2a08f1ae
lyatar <yatar.lauren@gmail.com>
2017-12-06 21:18:59 -0800

    Fix

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var request = require("request");
 
+var port = process.env.PORT || 8080;
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
